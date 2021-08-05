352fbafbe1b5ee035397a7ccabe169192284976e
Onevliyaoglu <onevliyaoglu@hotmail.com>
2021-08-05 19:29:32 +0300

    Main

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch");
+const dfd = require("danfojs-node")
+getData();
+     async function getData() {
+
+          const response = await fetch('https://ftx.com/api/markets')
+          const data = await response.json();
+          console.table(data.result);
+          df = new dfd.DataFrame(data.result);
+          df.print();
+          df.to_csv("Data.csv")
+
+     }
